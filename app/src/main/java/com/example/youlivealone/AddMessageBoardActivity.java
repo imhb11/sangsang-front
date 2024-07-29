153a314f10b8e2dfafcfcd6bfe75553d9a6bc230
chan <qmfhzhffl22@naver.com>
2024-07-30 01:07:00 +0900

    게시판 화면 추가

---
+++ app/src/main/java/com/example/youlivealone/AddMessageBoardActivity.java
@@ -0,0 +1,13 @@
+package com.example.youlivealone;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddMessageBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addmessageboard); // addmessageboard.xml 레이아웃 파일 참조
+    }
+}
