de0a34a70f8b15e63fe76ad6f358141e6ba9fb9a
chan <qmfhzhffl22@naver.com>
2024-07-30 15:34:22 +0900

    게시판 게시글 작성화면 추가

--- app/src/main/java/com/example/youlivealone/MessageBoard.java
+++ app/src/main/java/com/example/youlivealone/MessageBoard.java
@@ -1,5 +1,6 @@
 package com.example.youlivealone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +42,8 @@ public class MessageBoard extends AppCompatActivity {
         additionalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MessageBoard.this, AddMessageBoard.class);
+                startActivity(intent);
                 // 추가 기능 구현 (예: 새로운 게시물 추가)
             }
         });
