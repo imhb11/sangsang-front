8b5ce074ea29aaaa98bec2e7239eace00fcb2468
ehdals5598 <ehdals5598@naver.com>
2024-07-31 14:54:55 +0900

    커뮤니티 화면, 로그인, 회원가입 서버 연동(백과 협의하여 테스트 필요)

--- app/src/main/java/com/example/youlivealone/Login.java
+++ app/src/main/java/com/example/youlivealone/Login.java
@@ -7,10 +7,21 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Login extends AppCompatActivity {
-    TextView sign;
+    EditText ID, Password;
+    Button Login;
+    TextView sign, Signup;
     private static final int TIME_INTERVAL = 1500; // 뒤로가기 버튼을 누른 시간 간격 (1.5초)
     private long mBackPressedTime; // 뒤로가기 버튼을 누른 시간을 저장할 변수
 
@@ -18,6 +29,59 @@ public class Login extends AppCompatActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        ID = findViewById(R.id.editID);
+        Password = findViewById(R.id.ediPassword);
+        Login = findViewById(R.id.loginbutton);
+        Signup = findViewById(R.id.signin);
+
+        //로그인 버튼 이벤트
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = ID.getText().toString();
+                String pw = Password.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            if (success)
+                            {
+                                String msg = jsonObject.getString("ID");
+                                Toast.makeText(getApplicationContext(), "로그인 성공. ID :" + msg, Toast.LENGTH_SHORT).show();
+                            }
+
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "예외 1", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequestActivity loginRequestActivity = new LoginRequestActivity(id, pw, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                queue.add(loginRequestActivity);
+            }
+        });
 
         //회원가입 버튼 누르면 회원가입 페이지로 이동
         findViewById(R.id.signin).setOnClickListener(v -> {
--- app/src/main/java/com/example/youlivealone/Register.java
+++ app/src/main/java/com/example/youlivealone/Register.java
@@ -7,17 +7,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Register extends AppCompatActivity {
     TextView back;
-    EditText name,id,pw,pw2,email,birthyear,birthdate,birthday;
-    Button pwcheck, submit;
+    EditText name,ID,PW,pw2,email,birthyear,birthdate,birthday;
+    Button pwcheck, submit, signupbutton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+
+        submit = findViewById(R.id.signupbutton);
+        ID = (EditText) findViewById(R.id.signID);
+        PW = (EditText) findViewById(R.id.signPW);
 
         //뒤로 가기 버튼
         back = findViewById(R.id.back);
@@ -25,8 +40,6 @@ public class Register extends AppCompatActivity {
 
         //기입 항목
         name = findViewById(R.id.signName);
-        id=findViewById(R.id.signID);
-        pw=findViewById(R.id.signPW);
         pw2=findViewById(R.id.signPW2);
         email=findViewById(R.id.signmail);
         birthyear=findViewById(R.id.signBirth);
@@ -36,15 +49,69 @@ public class Register extends AppCompatActivity {
         //비밀번호 확인 버튼
         pwcheck = findViewById(R.id.pwcheckbutton);
         pwcheck.setOnClickListener(v -> {
-            if(pw.getText().toString().equals(pw2.getText().toString())){
+            if(PW.getText().toString().equals(pw2.getText().toString())){
                 pwcheck.setText("일치");
             }else{
                 Toast.makeText(Register.this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
             }
         });
 
+        //회원가입 버튼 이벤트
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String id = ID.getText().toString();
+                String pw = PW.getText().toString();
+                String Name = name.getText().toString();
+                String Email = email.getText().toString();
+                String Birthdate = birthdate.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            if (success)
+                            {
+                                Toast.makeText(getApplicationContext(), "성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Register.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+
+                        catch(JSONException e)
+                        {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "예외 1", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                SignupRequestActivity signupRequestActivity = new SignupRequestActivity(Name, id, Birthdate, pw, Email, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                queue.add(signupRequestActivity);
+            }
+        });
+
+
+
         //회원가입 완료 버튼
-        submit = findViewById(R.id.signupbutton);
+
         submit.setOnClickListener(v -> {
             Intent intent = new Intent(this, Login.class);
             startActivity(intent);
