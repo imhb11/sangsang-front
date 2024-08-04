6a4d52863b2a808fc0e9d8879adfda7d967d359f
chan <qmfhzhffl22@naver.com>
2024-08-04 18:59:14 +0900

    계정설정 액티비티, 알림설정 액티비티 등록

---
+++ app/src/main/java/com/example/youlivealone/AccountSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.youlivealone;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountSettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings);
+        // 계정 설정 관련 코드 작성
+    }
+}
