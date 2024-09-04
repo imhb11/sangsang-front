5e7b0a47cf6c3233f9a4d2753c35ea50783e9119
dhwlddj22 <qmfhzhffl22@naver.com>
2024-09-04 21:33:25 +0900

    마이 페이지 각 기능들 추가한 것.

---
+++ app/src/main/java/com/example/youlivealone/GroupActivity.java
@@ -0,0 +1,15 @@
+package com.example.youlivealone;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+
+        // 필요한 초기화 코드 작성
+    }
+}
