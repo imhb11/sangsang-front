946bbf9f1d513d74086822988d394191855233b0
ehdals5598 <ehdals5598@naver.com>
2024-09-11 00:16:33 +0900

    박동민작업내용추가

---
+++ OpenCV/build/intermediates/cxx/Debug/2x6j294r/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\user\\AndroidStudioProjects\\sangsang-front\\OpenCV\\.cxx\\Debug\\2x6j294r\\x86" ^
+  opencv_jni_shared
