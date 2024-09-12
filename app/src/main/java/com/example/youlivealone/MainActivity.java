b902e8ab2b3343e4d28baacad4d1a19947aa8256
임혜빈 <hywu5464@naver.com>
2024-09-12 09:55:33 +0900

    오류해결?

--- app/src/main/java/com/example/youlivealone/MainActivity.java
+++ app/src/main/java/com/example/youlivealone/MainActivity.java
@@ -1,26 +1,36 @@
 package com.example.youlivealone;
 
-import android.os.Bundle;
-import android.content.Intent;
-import android.widget.Toast;
-import android.os.Handler;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-import java.util.Arrays;
-import java.util.List;
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import java.util.Calendar;
-import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.youlivealone.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int TIME_INTERVAL = 1500; // 뒤로가기 버튼을 누른 시간 간격 (1.5초)
+    private long mBackPressedTime; // 뒤로가기 버튼을 누른 시간을 저장할 변수
+
+    private ActivityMainBinding mBinding;
+    private Handler sliderHandler = new Handler();
+
     private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                 if (isGranted) {
@@ -30,13 +40,12 @@ public class MainActivity extends AppCompatActivity {
                     // 사용자가 알림 권한을 거부함
                 }
             });
-    private static final int TIME_INTERVAL = 1500; // 뒤로가기 버튼을 누른 시간 간격 (1.5초)
-    private long mBackPressedTime; // 뒤로가기 버튼을 누른 시간을 저장할 변수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
         // Android 13 이상에서는 알림 권한이 필요한지 확인하고 요청
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -48,143 +57,145 @@ public class MainActivity extends AppCompatActivity {
         } else {
             setDailyAlarm(); // Android 13 이하에서는 권한 요청 필요 없음
         }
-    }
 
-    private void requestNotificationPermission() {
-        // 권한 요청
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.POST_NOTIFICATIONS)) {
-            // 권한 설명을 제공하고 요청
-            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
-        } else {
-            // 바로 권한 요청
-            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
-        }
-    }
-
-    private void setDailyAlarm() {
-
-        // 매일 알림을 보낼 시간 설정
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 10);
-        calendar.set(Calendar.SECOND, 0);
-
-        // AlarmManager 설정
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // 매일 같은 시간에 알람 설정
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                AlarmManager.INTERVAL_DAY, pendingIntent);
-
-        //이미지 슬라이드 코드
-
-        ViewPager2 viewPager = findViewById(R.id.viewPager);
-
+        // 이미지 슬라이드 코드
+        ViewPager2 viewPager2 = mBinding.viewPager;
         List<Integer> images = Arrays.asList(
                 R.drawable.image1,
                 R.drawable.image2,
                 R.drawable.image3,
                 R.drawable.image4
         );
+        ImageAdapter adapter = new ImageAdapter(images, viewPager2);
+        viewPager2.setAdapter(adapter);
 
-        viewPager.setAdapter(new ImageAdapter(images));
-
-
-        //이미지 슬라이드 코드 종료
-
-
-        //버튼 작동코드들
-
-        findViewById(R.id.setting).setOnClickListener(v -> {
-            Intent goSetting = new Intent(MainActivity.this, SettingActivity.class);
-            startActivity(goSetting);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable, 2000);
+            }
         });
 
-        findViewById(R.id.notice).setOnClickListener(v -> {
-            Intent goNotice = new Intent(MainActivity.this, Notice.class);
-            startActivity(goNotice);
+        // 버튼 작동 코드들
+        mBinding.setting.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.honeytip).setOnClickListener(v -> {
-            Intent goHoneytip = new Intent(MainActivity.this, Honeytip.class);
-            startActivity(goHoneytip);
+        mBinding.notice.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Notice.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.purchase).setOnClickListener(v -> {
-            Intent goPurchase = new Intent(MainActivity.this, Purchase.class);
-            startActivity(goPurchase);
+        mBinding.honeytip.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Honeytip.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.meeting).setOnClickListener(v -> {
-            Intent goMeeting = new Intent(MainActivity.this, Meeting.class);
-            startActivity(goMeeting);
+        mBinding.purchase.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Purchase.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.community).setOnClickListener(v -> {
-            Intent goCommunity = new Intent(MainActivity.this, Community.class);
-            startActivity(goCommunity);
+        mBinding.meeting.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Meeting.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.smart).setOnClickListener(v -> {
-            Intent goSmart = new Intent(MainActivity.this, Smart.class);
-            startActivity(goSmart);
+        mBinding.community.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Community.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.recycle).setOnClickListener(v -> {
-            Intent goRecycle = new Intent(MainActivity.this, Recycle.class);
-            startActivity(goRecycle);
+        mBinding.smart.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Smart.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.map).setOnClickListener(v -> {
-            Intent goMap = new Intent(MainActivity.this, Map.class);
-            startActivity(goMap);
+        mBinding.recycle.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Recycle.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.check).setOnClickListener(v -> {
-            Intent goCheck = new Intent(MainActivity.this, Check.class);
-            startActivity(goCheck);
+        mBinding.map.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Map.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.home).setOnClickListener(v -> {
-            Intent goHome = new Intent(MainActivity.this, MainActivity.class);
-            startActivity(goHome);
+        mBinding.check.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Check.class);
+            startActivity(intent);
         });
 
-        findViewById(R.id.chat).setOnClickListener(v -> {
-            Intent goChat = new Intent(MainActivity.this, Chat.class);
-            startActivity(goChat);
+        mBinding.home.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
         });
-        findViewById(R.id.mypage).setOnClickListener(v -> {
-            Intent goMypage = new Intent(MainActivity.this, Mypage.class);
-            startActivity(goMypage);
+
+        mBinding.chat.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Chat.class);
+            startActivity(intent);
+        });
+
+        mBinding.mypage.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Mypage.class);
+            startActivity(intent);
         });
     }
+
+    // 알림 권한 요청 처리
+    private void requestNotificationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.POST_NOTIFICATIONS)) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+        }
+    }
+
+    // 매일 알림 설정
+    private void setDailyAlarm() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 10);
+        calendar.set(Calendar.SECOND, 0);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
+
     @Override
     public void onBackPressed() {
-        // 현재 시간 가져옴
         long currentTime = System.currentTimeMillis();
 
-        // 뒤로가기 버튼을 처음 눌렀을 때
         if (mBackPressedTime + TIME_INTERVAL > currentTime) {
             super.onBackPressed();
-        } else { // 뒤로가기 버튼 처음 눌렀거나 시간 간격을 초과
+        } else {
             Intent intent = new Intent(MainActivity.this, MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             startActivity(intent);
             final Toast toast = Toast.makeText(this, "한 번 더 누르면 종료됩니다", Toast.LENGTH_SHORT);
             toast.show();
 
-            //TIME_INTERVAL 만큼 토스트 띄우기
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    toast.cancel();
-                }
-            }, TIME_INTERVAL);
+            new Handler().postDelayed(toast::cancel, TIME_INTERVAL);
         }
-        // 뒤로가기 버튼 누른 시간을 업데이트
         mBackPressedTime = currentTime;
     }
+
+    private final Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            int nextItem = mBinding.viewPager.getCurrentItem() + 1;
+            if (nextItem >= mBinding.viewPager.getAdapter().getItemCount()) {
+                nextItem = 0;
+            }
+            mBinding.viewPager.setCurrentItem(nextItem, true);
+        }
+    };
+
+
 }
